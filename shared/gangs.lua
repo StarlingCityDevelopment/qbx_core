5cc050f0ac7b45c45724ae9b959f070a27657c53
Codexis <contact@codexis.dev>
2025-08-30 15:33:50 +0200

    feat(shared): added new entities

--- shared/gangs.lua
+++ shared/gangs.lua
@@ -9,4 +9,85 @@ return {
             },
         },
 	},
+
+    ['yakuza'] = {
+		label = 'Yakuza',
+		grades = {
+            [0] = {
+                name = 'Shatei',
+            },
+            [1] = {
+                name = 'Kyodai',
+            },
+            [2] = {
+                name = 'Wakagashira',
+            },
+            [3] = {
+                name = 'Oyabun',
+            },
+        },
+	},
+
+    ['blackdynasty'] = {
+		label = 'Black Dynasty',
+		grades = {
+            [0] = {
+                name = 'Habitant',
+            },
+            [1] = {
+                name = 'Gangster',
+            },
+            [2] = {
+                name = 'Grand',
+            },
+            [3] = {
+                name = 'Double OG',
+            },
+            [4] = {
+                name = 'OG',
+            },
+        },
+	},
+
+    ['gangsterdynasty'] = {
+		label = 'Gangster Dynasty',
+		grades = {
+            [0] = {
+                name = 'Habitant',
+            },
+            [1] = {
+                name = 'Gangster',
+            },
+            [2] = {
+                name = 'Grand',
+            },
+            [3] = {
+                name = 'Double OG',
+            },
+            [4] = {
+                name = 'OG',
+            },
+        },
+	},
+
+    ['cobradiscipline'] = {
+		label = 'Cobra Disciple',
+		grades = {
+            [0] = {
+                name = 'Habitant',
+            },
+            [1] = {
+                name = 'Gangster',
+            },
+            [2] = {
+                name = 'Grand',
+            },
+            [3] = {
+                name = 'Double OG',
+            },
+            [4] = {
+                name = 'OG',
+            },
+        },
+	},
 }
